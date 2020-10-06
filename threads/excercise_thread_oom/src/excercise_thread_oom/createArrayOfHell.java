b858ae3c2953cfc4e585157bd9d9f13d58221b3c
HendrikWunsch <hendrik.wunsch@cofinpro.de>
2020-10-06 21:09:14 +0200

    add project threads

---
+++ threads/excercise_thread_oom/src/excercise_thread_oom/createArrayOfHell.java
@@ -0,0 +1,20 @@
+package excercise_thread_oom;
+
+import java.util.Random;
+
+public class createArrayOfHell implements Runnable{
+
+	int min = 1000000000;
+	
+	
+	@Override
+	public void run() {
+		Random r = new Random();
+		int[] intA = new int[r.nextInt(min)+min];
+		intA[0] = r.nextInt(min)+min;
+		System.out.println("Das Array enthält: " + intA[0]);
+		
+		
+	}
+
+}
