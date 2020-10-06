d89686db09c9a85e75819156f0da1e162b862cd2
HendrikWunsch <hendrik.wunsch@cofinpro.de>
2020-10-06 21:25:54 +0200

    Zwischenstand für Stufe 4.
    Stufe 4: Verwende das Java.concurrent Package, um die Anzahl der
    gestarten Threads zu begrenzen
    A. Selber was erstellen
    B. Nutze ein gegebenes Konstrukt

--- threads/excercise_thread_oom/src/excercise_thread_oom/demoThreadOom.java
+++ threads/excercise_thread_oom/src/excercise_thread_oom/demoThreadOom.java
@@ -2,18 +2,24 @@ package excercise_thread_oom;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class demoThreadOom {
 
 	public static void main(String[] args) {
+		ExecutorService executor = Executors.newFixedThreadPool(1);
 
 		createArrayOfHell c = new createArrayOfHell();
 		List<createArrayOfHell> list = new ArrayList<createArrayOfHell>();
 		for (int i = 0; i <= 100; i++) {
 			list.add(c);
 		}
-		list.forEach(t -> {
-			t.run();
+		
+		executor.submit(() -> {
+			list.forEach(t -> {
+				t.run();
+			});
 		});
 
 	}
