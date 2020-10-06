512d274a4e167830a6aef39ed9000407d3613c22
HendrikWunsch <hendrik.wunsch@cofinpro.de>
2020-10-06 21:22:49 +0200

    Enthält die folgenden Schritte:
    1. Implementiere ein Runnable mit einem Array und Laufzeit von 10
    Sekunden
    2. Initiiere 100 Stück davon
    3. Skaliere das Array, die Größe so groß dass es bei 100 Threads zu
    einer out of Memory exception kommt

--- threads/excercise_thread_oom/src/excercise_thread_oom/createArrayOfHell.java
+++ threads/excercise_thread_oom/src/excercise_thread_oom/createArrayOfHell.java
@@ -2,19 +2,17 @@ package excercise_thread_oom;
 
 import java.util.Random;
 
-public class createArrayOfHell implements Runnable{
+public class createArrayOfHell implements Runnable {
 
 	int min = 1000000000;
-	
-	
+
 	@Override
 	public void run() {
 		Random r = new Random();
-		int[] intA = new int[r.nextInt(min)+min];
-		intA[0] = r.nextInt(min)+min;
+		int[] intA = new int[r.nextInt(min) + min];
+		intA[0] = r.nextInt(min) + min;
 		System.out.println("Das Array enthält: " + intA[0]);
-		
-		
+
 	}
 
 }
--- threads/excercise_thread_oom/src/excercise_thread_oom/demoThreadOom.java
+++ threads/excercise_thread_oom/src/excercise_thread_oom/demoThreadOom.java
@@ -2,24 +2,18 @@ package excercise_thread_oom;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class demoThreadOom {
 
 	public static void main(String[] args) {
-		ExecutorService executor = Executors.newFixedThreadPool(1);
 
 		createArrayOfHell c = new createArrayOfHell();
 		List<createArrayOfHell> list = new ArrayList<createArrayOfHell>();
 		for (int i = 0; i <= 100; i++) {
 			list.add(c);
 		}
-		
-		executor.submit(() -> {
-			list.forEach(t -> {
-				t.run();
-			});
+		list.forEach(t -> {
+			t.run();
 		});
 
 	}
